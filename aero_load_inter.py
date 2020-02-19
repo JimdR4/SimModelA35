156110ea6c4c45491593c1570c12b629d9ea61c7
FVossen <61149685+FVossen@users.noreply.github.com>
2020-02-19 11:49:40 +0100

    Add files via upload

--- aero_load_inter.py
+++ aero_load_inter.py
@@ -2,16 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def aero_load_interpolation(direction):
-    ''' Input:  direction: choose between 'x' or 'z' 
-        Output: 1. coeff: coefficients of polynomial; 41 coefficients when choosing 'x' 
-                          & 81 coefficients when choosing 'z' 
-                2.q_z or q_x: vector of all point forces in spanwise direction at constant chord segment location 
-                              OR vector of all point forces in chordwise direction at constant span segment location
-                3. x_co_q or z_co_q: x coordinate where point loads act, which is in the center of each segment 
-                                     OR z coordinate where point loads act, which is in the center of each segment
+    ''' Input:  direction:  choose between 'x' or 'x_torque' 
+        Output: 1. coeff:   coefficients of polynomial; 41 coefficients
+                2. q_x or q_x_torque:   vector of all point forces in spanwise direction at constant chord segment location 
+                                        OR vector of all point torques in spanwise direction at constant chord segment location
+                3. x_co_q: x coordinate where point loads act, which is in the center of each segment 
     '''
 
-    aero_data = '/Users/frans/Desktop/AE/AE3212_II-Simulation_Varification_and_Validation/Assignment/Data/aerodynamicloaddo228.dat' # directory for the aero data set
+    aero_data = '/Users/frans/Desktop/AE/AE3212_II-Simulation_Varification_and_Validation/Python/Aero_load/aerodynamicloaddo228.dat'
     data = np.genfromtxt(aero_data,delimiter=',') # column is spanwise // row is chordwise
     # b stands for span
     # c stands for chord
@@ -24,6 +22,8 @@ def aero_load_interpolation(direction):
 
     dc = Ca/Nc  # segment width in chordwise direction
     db = la/Nb  # segment width in spanwise direction
+
+    sc_z = -0.2 # assumption for now / chord runs from 0 to -0.515
 
     if direction == 'x':
         # integrate along chordline for all spanwise segment
@@ -49,34 +49,38 @@ def aero_load_interpolation(direction):
             A[:,column] = x_co_q**column
 
         coeff = np.linalg.solve(A,q_x)
+
         return coeff, q_x, x_co_q
-        
-    
-    if direction == 'z':
-       # integrate along span for all chord segment
-        q_z = np.zeros(Nc)
 
-        for c_i in range(Nc):
-            q_along_b = data[c_i,:]         # vector of all point forces in spanwise direction at same chord segment location 
-            q_z[c_i]  = sum(q_along_b * db) # numerical integration
+    if direction == 'x_torque':
+        # integrate along chordline for all spanwise segment
+        q_x_torque = np.zeros(Nb)
 
-        z_co_q = np.arange(dc/2,Ca,dc)  # Z coordinate where point loads act, which is in the center of each segment
+        z_co_q = -1 * np.arange(dc/2,Ca,dc)      # negative z coordinate where point loads act, which is in the center of each segment along the chord
+
+        for b_i in range(Nb):
+            q_along_c  = data[:,b_i]                      # vector of all point forces in chordwise direction at same spanwise segment location 
+            shear_dist = z_co_q - sc_z                    # array with all distances from segment location to shear center
+            q_x_torque[b_i]   = sum(q_along_c * dc * shear_dist) # numerical integration of torque along the chord 
+
+        x_co_q = np.arange(db/2,la,db)      # x coordinate where point loads act, which is in the center of each segment
 
         # *** interpolation ***
-        #       Assume as solution,     w(z) = a0 + a1*z + a2*z^2 + a3*z^3 + ... + a80*z^80
-        #       Coefficients can be found by solving,   A /cdot coeff = q_z
-        #           with A = [ 1   , z0  , z0^2  , z0^3  , ... , z0^40  ]
-        #                    [ 1   , z1  , z1^2  , z1^3  , ... , z1^40  ]
+        #    Assume as solution,     w(x) = a0 + a1*x + a2*x^2 + a3*x^3 + ... + a40*x^40
+        #    Coefficients can be found by solving,   A /cdot coeff = q_x
+        #           with A = [ 1   , x0  , x0^2  , x0^3  , ... , x0^40  ]
+        #                    [ 1   , x1  , x1^2  , x1^3  , ... , x1^40  ]
         #                    [ ... ,                           , ...    ]
-        #                    [ 1   , z40 , z40^2 , z40^3 , ... , z40^40 ]
-
-        A = np.zeros((Nc,Nc))            
+        #                    [ 1   , x40 , x40^2 , x40^3 , ... , x40^40 ]
+                
+        A = np.zeros((Nb,Nb))            
         A[:,0] = 1
-        for column in range(1,Nc):
-            A[:,column] = z_co_q**column
+        for column in range(1,Nb):
+            A[:,column] = x_co_q**column
 
-        coeff = np.linalg.solve(A,q_z)    
-        return coeff, q_z, z_co_q
+        coeff = np.linalg.solve(A,q_x_torque)
+
+        return coeff, q_x_torque, x_co_q
 
 def w(coordinates,coeff,direction):
     ''' input:  coordinates: values used in the interpolated function
@@ -86,12 +90,11 @@ def w(coordinates,coeff,direction):
     '''
     
     Nb = 41     # number of spanwise segments
-    Nc = 81     # number of chordwise segments
 
     if direction == 'x':
         N = Nb
-    if direction == 'z':
-        N = Nc
+    if direction == 'x_torque':
+        N = Nb
 
     if np.size(coordinates) >1: # when there is more then 1 coordinate
         sol = []
@@ -113,7 +116,7 @@ def w(coordinates,coeff,direction):
 
     return sol
 
-direction = 'x' # choose between 'z' OR 'x'
+direction = 'x_torque' # choose between 'x' OR 'x_torque'
 coeff, q, co = aero_load_interpolation(direction)
 
 x = co
