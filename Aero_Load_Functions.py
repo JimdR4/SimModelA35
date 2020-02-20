17a8ad954f730ab1fa559ae2fc9261aa0674cd59
FVossen <61149685+FVossen@users.noreply.github.com>
2020-02-20 14:30:18 +0100

    Add files via upload

--- Aero_Load_Functions.py
+++ Aero_Load_Functions.py
@@ -1,5 +1,4 @@
 import numpy as np 
-# from matplotlib import pyplot as plt
 
 def aero_load_interpolation(force_or_torque):
     ''' Input:  force_or_torque =   Choose if you want to find the coefficients for the interpolant q(x) ['force'] or the coefficients for the interpolant t(x) ['shear']
@@ -8,7 +7,7 @@ def aero_load_interpolation(force_or_torque):
                 x_dom =             This is the x domain over which the function is interpolated
     '''
     
-    aero_data = '/Users/frans/Desktop/AE/AE3212_II-Simulation_Varification_and_Validation/Python/aerodynamicloaddo228.dat'
+    aero_data = '/Users/frans/Desktop/AE/AE3212_II-Simulation_Varification_and_Validation/Python/Local/aerodynamicloaddo228.dat'
     data = np.genfromtxt(aero_data,delimiter=',') # column is spanwise // row is chordwise
 
     ''' Input variables '''
@@ -22,9 +21,9 @@ def aero_load_interpolation(force_or_torque):
     # Constants
     dc = Ca/Nc                          # segment width in chordwise direction
     db = la/Nb                          # segment width in spanwise direction
-    z_ax = -1 * np.arange(dc/2, Ca, dc) # array containing all z coordinates where a point load q acts
-    x_dom = np.arange(db/2, la, db)      # array containing all x coordinates where a point load q acts
-    shear_dist = z_ax - sc_z
+
+    x_dom, z_dom = aero_co()
+    shear_dist = z_dom - sc_z
 
     #---------- Make choice between computing   q(x) needed for Mz      --> 'force'
     #                                           t(z) needed for Torque  --> 'torque'
@@ -34,7 +33,7 @@ def aero_load_interpolation(force_or_torque):
         for bi in range(Nb):                # repeating process for each spanwise location
             q_cs = data[:,bi]               # array containing all point loads along the chord for a spanwise location
             # Integrating q(z)
-            q_c[bi] = simpsons_rule_integration(z_ax,Nc,q_cs)
+            q_c[bi] = simpsons_rule_integration(z_dom,Nc,q_cs)
 
         # Interpolation to obtain q(x)
         # Assume as solution,     q(x) = a0 + a1*x + a2*x^2 + a3*x^3 + ... + a40*x^40
@@ -47,7 +46,7 @@ def aero_load_interpolation(force_or_torque):
         A = np.ones((Nb,Nb))
         for column in range(1,Nb):
             A[:,column] = x_dom**column
-        coeff = np.linalg.solve(A,q_c) # coefficients of the function // q(z) = a0 + a1*z + a2*z^2 + ... + a40*z^40
+        coeff = np.linalg.solve(A,q_c) # coefficients of the function // q(z) = a0 + a1*z + a2*z^2 + ... + a60*z^60
 
         return coeff, q_c, x_dom
     
@@ -58,7 +57,7 @@ def aero_load_interpolation(force_or_torque):
         for bi in range(Nb):
             t_cs = data[:,bi] * shear_dist
             # Integrating t(z)
-            t_c[bi] = simpsons_rule_integration(z_ax, Nc, t_cs)
+            t_c[bi] = simpsons_rule_integration(z_dom, Nc, t_cs)
 
         # Interpolation to obtain t(x)
         # Assume as solution,     t(x) = a0 + a1*x + a2*x^2 + a3*x^3 + ... + a40*x^40
@@ -71,7 +70,7 @@ def aero_load_interpolation(force_or_torque):
         A = np.ones((Nb,Nb))
         for column in range(1,Nb):
             A[:,column] = x_dom**column
-        coeff = np.linalg.solve(A,t_c) # coefficients of the function // t(z) = a0 + a1*z + a2*z^2 + ... + a40*z^40
+        coeff = np.linalg.solve(A,t_c) # coefficients of the function // q(z) = a0 + a1*z + a2*z^2 + ... + a60*z^60
 
         return coeff, t_c, x_dom
 
@@ -83,7 +82,7 @@ def aero_load_function(coordinates, coeff, direction):
     '''
     
     Nb = 41     # number of spanwise segments
-    Nc = 81     # number of chordwise segments
+    Nc = 81
 
     if direction == 'x':
         N = Nb
@@ -115,7 +114,7 @@ def aero_load_1_int_function(coordinates, coeff, direction):
     '''
     
     Nb = 41     # number of spanwise segments
-    Nc = 81     # number of chordwise segments
+    Nc = 81
 
     if direction == 'x':
         N = Nb
@@ -151,7 +150,7 @@ def aero_load_2_int_function(coordinates, coeff, direction):
     '''
     
     Nb = 41     # number of spanwise segments
-    Nc = 81     # number of chordwise segments
+    Nc = 81
 
     if direction == 'x':
         N = Nb
@@ -191,7 +190,7 @@ def aero_load_3_int_function(coordinates, coeff, direction):
     '''
     
     Nb = 41     # number of spanwise segments
-    Nc = 81     # number of chordwise segments
+    Nc = 81
 
     if direction == 'x':
         N = Nb
@@ -250,22 +249,35 @@ def simpsons_rule_integration(coordinates, n, f):
     
     return int
 
-# for testing
+def aero_co():
+    #x and z discretization
+    Nz = 81
+    Nx = 41
+    la = 2.691
+    Ca = 0.515
 
-# coeff, q, x_dom = aero_load_interpolation('torque')
+    xdisc = np.zeros(Nx)
+    #determine the x locations of the distcretization of q(x,z)
+    for i in range(1,Nx):
+        #calculate theta_(x,i)
+        thetaxi = ((i-1)/Nx)*np.pi
+        #calculate theta_(x,i+1)
+        thetaxi2 = ((i)/Nx)*np.pi
+        #calculate x_i
+        xi = 0.5 * ((la/2)*(1-np.cos(thetaxi))+(la/2)*(1-np.cos(thetaxi2)))
+        xdisc[i] = xi
+    xdisc = xdisc
+    
+    zdisc = np.zeros(Nz)
+    #determine the y locations of the distcretization of q(x,z)
+    for i in range(1,Nz):
+        #calculate theta_(z,i)
+        thetazi = ((i-1)/Nz)*np.pi
+        #calculate theta_(z,i+1)
+        thetazi2 = ((i)/Nz)*np.pi
+        #calculate z_i
+        zi = 0.5 * ((Ca/2)*(1-np.cos(thetazi))+(Ca/2)*(1-np.cos(thetazi2)))
+        zdisc[i] = zi
+    zdisc = -1 * zdisc
 
-# I = []
-# for n in range(3,200,2):
-#     x = np.linspace(x_dom[0],x_dom[-1],n)
-#     y = aero_load_function(x, coeff, 'x')
-#     y1 = aero_load_1_int_function(x, coeff, 'x')
-#     y2 = aero_load_2_int_function(x, coeff, 'x')
-
-#     Ii = simpsons_rule_integration(x,n,y2)
-#     I.append(Ii)
-
-# plt.plot(x,y,'bo',x,y,'k')
-# plt.plot(x,y1,'bo',x,y1,'k')
-# plt.plot(x,y2,'bo',x,y2,'k')
-# plt.grid()
-# plt.show()
+    return xdisc, zdisc
