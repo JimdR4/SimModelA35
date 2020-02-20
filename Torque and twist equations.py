83a4b412ea93d6f08631da47d9549570876f8fdc
sdevilder <61149798+sdevilder@users.noreply.github.com>
2020-02-20 16:29:12 +0100

    Add files via upload
    without integrals

---
+++ Torque and twist equations.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 15:24:16 2020
+
+@author: Sietse de Vilder
+"""
+
+#VERIFY THIS!!!!
+#INCLUDE q(x,z)!!!!
+
+#coordinate system in the chord line
+#at the wing's leading edge
+
+#Trial inputs:
+
+P = 20.6 * 1000 #N
+
+import numpy as np
+
+def T_x(R1y,R2y,R3y,R_I,z_hat,x,P):
+     
+    #Defining all the x locations:
+    x1 = 0.174 #m
+    x2 = 1.051
+    x3 = 2.512
+    xa = 0.3
+    theta = 0.436332 #[rad], also, we assume constant theta, since initial
+    #deflection angle is way larger than deflection
+    #due twist
+    h_a = 0.248 #[m], airfoil height
+    #z_hat is the z location of the shear center
+    #Doing Macaulay step function for the torque T(x):
+    
+    if x<x1:
+        return 0# - int int q(x,z) * (z-z_hat)
+    
+    elif (x2-0.5*xa)>x>=x1:
+        return -R1y * (-0.5* h_a-z_hat) # + int int q(x,z) * (z-z_hat) 
+    
+    elif x2>x>=(x2-0.5*xa):
+        return -R1y * (-0.5* h_a-z_hat) - R_I * np.sin(theta)* (-z_hat) + R_I * np.cos(theta) * 0.5 *h_a  # + int int q(x,z) * (z-z_hat) 
+    
+    elif (x2+0.5*xa)>x>=x2:
+        return -R1y * (-0.5* h_a-z_hat) - R_I * np.sin(theta)* (-z_hat) + R_I * np.cos(theta) * 0.5 *h_a - R2y * (-0.5*h_a-z_hat)  # + int int q(x,z) * (z-z_hat) 
+    
+    elif x3>x>=(x2+0.5*xa):
+        return -R1y * (-0.5* h_a-z_hat) - R_I * np.sin(theta)* (-z_hat) + R_I * np.cos(theta) * 0.5 *h_a - R2y * (-0.5*h_a-z_hat) \
+                + P *np.sin(theta) * (-z_hat) - P * np.cos(theta) * (0.5*(h_a) ) # + int int q(x,z) * (z-z_hat)
+    
+    elif x >= x3 :
+        return -R1y * (-0.5* h_a-z_hat) - R_I * np.sin(theta)* (-z_hat) + R_I * np.cos(theta) * 0.5 *h_a - R2y * (-0.5*h_a-z_hat) \
+                + P *np.sin(theta) * (-z_hat) - P * np.cos(theta) * (0.5*h_a) -R3y * (-0.5*h_a-z_hat)  # + int int q(x,z) * (z-z_hat)
+    
+    
+    
+def Twist_x(G,J,R1y,z_hat,R2y,R3y,P,R_I,x):
+   
+    #Defining all the x locations:
+    x1 = 0.174 #m
+    x2 = 1.051
+    x3 = 2.512
+    xa = 0.3
+    theta = 0.436332 #[rad], also, we assume constant theta, since initial
+    #deflection angle is way larger than deflection
+    #due twist
+    h_a = 0.248 #[m], airfoil height
+    #z_hat is the z location of the shear center
+    #Doing Macaulay step function for the torque T(x):
+    
+    if x<x1:
+        return (1/(G*J))*doubleintegral + C5
+    
+    elif (x2-0.5*xa)>x>=x1:
+        return (1/(G*J))*(-R1y*(x-x1)*(-0.5* h_a-z_hat) + doubleintegral) + C5 
+    
+    elif x2>x>=(x2-0.5*xa):
+        return (1/(G*J))*(-R1y*(x-x1)*(-0.5* h_a-z_hat) - R_I*np.sin(theta)*(x-x2+xa/2)*(-z_hat) + R_I*np.cos(theta)*(x-x2+xa/2)*0.5*h_a + doubleintegral) + C5
+    
+    elif (x2+0.5*xa)>x>=x2:
+        return (1/(G*J))*(-R1y*(x-x1)*(-0.5* h_a-z_hat) - R_I*np.sin(theta)*(x-x2+xa/2)*(-z_hat) + R_I*np.cos(theta)*(x-x2+xa/2)*0.5*h_a - R2y*(x-x2)*(-0.5*h_a-z_hat) + doubleintegral)  + C5
+    
+    elif x3>x>=(x2+0.5*xa):
+        return (1/(G*J))*(-R1y*(x-x1)*(-0.5* h_a-z_hat) - R_I*np.sin(theta)*(x-x2+xa/2)*(-z_hat) + R_I*np.cos(theta)*(x-x2+xa/2)*0.5*h_a - R2y*(x-x2)*(-0.5*h_a-z_hat) \
+                + P*np.sin(theta)*(x-x2-xa/2)*(-z_hat) - P*np.cos(theta)*(x-x2-xa/2)*(0.5*(h_a)) + doubleintegral) + C5
+    
+    elif x >= x3 :
+        return (1/(G*J))*(-R1y*(x-x1)*(-0.5* h_a-z_hat) - R_I*np.sin(theta)*(x-x2+xa/2)*(-z_hat) + R_I*np.cos(theta)*(x-x2+xa/2)*0.5*h_a - R2y*(x-x2)*(-0.5*h_a-z_hat) \
+                + P*np.sin(theta)*(x-x2-xa/2)*(-z_hat) - P*np.cos(theta)*(x-x2-xa/2)*(0.5*(h_a)) -R3y(x-x3)*(-0.5*h_a-z_hat) + doubleintegral) + C5
+    
+   
