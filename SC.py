5d73f34aedcc073ad7d25daf7bcbf842ebb37141
MartinvanSchie <61149670+MartinvanSchie@users.noreply.github.com>
2020-02-24 14:23:58 +0100

    Update SC.py

--- SC.py
+++ SC.py
@@ -65,35 +65,58 @@ q6 = -q11
 
 #now for the SC:
 #up to boom 1, so 0<s<bstring
-S11 = -(Vy/Izz)*(-tskin*r*r*r*m.sin(s/r))
+sb = 0
+se = bstring
+S11 = -(Vy/Izz)*(-tskin*r*r*r*m.sin(se/r)) - (-(Vy/Izz)*(-tskin*r*r*r*m.sin(sb/r)))
 #from boom 1 to top of spar, so bstring<s<pi*r/2
-S1 = -(Vy/Izz)*(tskin*r*r*r*-m.sin(s/r) + astr*strloc[1,1]*s)+ q11*s
+sb = bstring
+se = m.pi*r/2
+S1 = -(Vy/Izz)*(tskin*r*r*r*-m.sin(se/r) + astr*strloc[1,1]*se)+ q11*se - (-(Vy/Izz)*(tskin*r*r*r*-m.sin(sb/r) + astr*strloc[1,1]*sb)+ q11*sb)
 #from top of spar to boom 2, so 0<s<(2*bstring-(pi*r/2))
-S32 = -(Vy/Izz)*(tskin*r*s*s/2-tskin*r*s*s*s/(6*l))+(q1+q2)*s
+sb = 0
+se = (2*bstring-(m.pi*r/2))
+S32 = -(Vy/Izz)*(tskin*r*se*se/2-tskin*r*se*se*se/(6*l))+(q1+q2)*se - (-(Vy/Izz)*(tskin*r*sb*sb/2-tskin*r*sb*sb*sb/(6*l))+(q1+q2)*sb)
 #from boom 2 to boom 3, so (2*bstring-(pi*r/2))<s<(3*bstring-(pi*r/2))
-S33 = -(Vy/Izz)*((tskin*r*s*s/2-tskin*r*s*s*s/(6*l)) + astr*strloc[2,1]*s)+q32*s
+sb = (2*bstring-(m.pi*r/2))
+se = (3*bstring-(m.pi*r/2))
+S33 = -(Vy/Izz)*((tskin*r*se*se/2-tskin*r*se*se*se/(6*l)) + astr*strloc[2,1]*se)+q32*se - (-(Vy/Izz)*((tskin*r*sb*sb/2-tskin*r*sb*sb*sb/(6*l)) + astr*strloc[2,1]*sb)+q32*sb)
 #from boom 3 to boom 4, so (3*bstring-(pi*r/2))<s<(4*bstring-(pi*r/2))
-S34 = -(Vy/Izz)*((tskin*r*s*s/2-tskin*r*s*s*s/(6*l)) + astr*strloc[3,1]*s)+q33*s
+sb = (3*bstring-(m.pi*r/2))
+se = (4*bstring-(m.pi*r/2))
+S34 = -(Vy/Izz)*((tskin*r*se*se/2-tskin*r*se*se*se/(6*l)) + astr*strloc[3,1]*se)+q33*se - (-(Vy/Izz)*((tskin*r*sb*sb/2-tskin*r*sb*sb*sb/(6*l)) + astr*strloc[3,1]*sb)+q33*sb)
 #from boom 4 to boom 5, so (4*bstring-(pi*r/2))<s<(5*bstring-(pi*r/2))
-S35 = -(Vy/Izz)*((tskin*r*s*s/2-tskin*r*s*s*s/(6*l)) + astr*strloc[4,1]*s)+q34*s
+sb = (4*bstring-(m.pi*r/2))
+se = (5*bstring-(m.pi*r/2))
+S35 = -(Vy/Izz)*((tskin*r*se*se/2-tskin*r*se*se*se/(6*l)) + astr*strloc[4,1]*se)+q34*se - (-(Vy/Izz)*((tskin*r*sb*sb/2-tskin*r*sb*sb*sb/(6*l)) + astr*strloc[4,1]*sb)+q34*sb)
 #from boom 4 to trailing edge, so (5*bstring-(pi*r/2))<s<l
-S3 = -(Vy/Izz)*((tskin*r*s*s/2-tskin*r*s*s*s/(6*l)) + astr*strloc[5,1]*s)+q35*s
+sb = (2*bstring-(m.pi*r/2))
+se = l
+S3 = -(Vy/Izz)*((tskin*r*se*se/2-tskin*r*se*se*se/(6*l)) + astr*strloc[5,1]*se)+q35*se - (-(Vy/Izz)*((tskin*r*sb*sb/2-tskin*r*sb*sb*sb/(6*l)) + astr*strloc[5,1]*sb)+q35*sb)
 #from trailing edge to boom 6, so 0<s<(6*bstring-(pi*r/2)-l)
-S46 = -(Vy/Izz)*(-tskin*r*s*s*s/(6*l))+q3*s
+sb = 0
+se = (6*bstring-(m.pi*r/2)-l)
+S46 = -(Vy/Izz)*(-tskin*r*se*se*se/(6*l))+q3*se - (-(Vy/Izz)*(-tskin*r*sb*sb*sb/(6*l))+q3*sb)
 #from boom 6 to boom 7, so (6*bstring-(pi*r/2)-l)<s<(7*bstring-(pi*r/2)-l)
-S47 = -(Vy/Izz)*(-tskin*r*s*s*s/(6*l) + astr*strloc[6,1]*s)+q46*s
+sb = (6*bstring-(m.pi*r/2)-l)
+se = (7*bstring-(m.pi*r/2)-l)
+S47 = -(Vy/Izz)*(-tskin*r*se*se*se/(6*l) + astr*strloc[6,1]*se)+q46*se - (-(Vy/Izz)*(-tskin*r*sb*sb*sb/(6*l) + astr*strloc[6,1]*sb)+q46*sb)
 #from boom 7 to boom 8, so (7*bstring-(pi*r/2)-l)<s<(8*bstring-(pi*r/2)-l)
-S48 = -(Vy/Izz)*(-tskin*r*s*s*s/(6*l) + astr*strloc[7,1]*s)+q47*s
+sb = (7*bstring-(m.pi*r/2)-l)
+se = (8*bstring-(m.pi*r/2)-l)
+S48 = -(Vy/Izz)*(-tskin*r*se*se*se/(6*l) + astr*strloc[7,1]*se)+q47*se - (-(Vy/Izz)*(-tskin*r*sb*sb*sb/(6*l) + astr*strloc[7,1]*sb)+q47*sb)
 #from boom 8 to boom 9, so (8*bstring-(pi*r/2)-l)<s<(9*bstring-(pi*r/2)-l)
-S49 = -(Vy/Izz)*(-tskin*r*s*s*s/(6*l) + astr*strloc[8,1]*s)+q48*s
+sb = (8*bstring-(m.pi*r/2)-l)
+se = (9*bstring-(m.pi*r/2)-l)
+S49 = -(Vy/Izz)*(-tskin*r*se*se*se/(6*l) + astr*strloc[8,1]*se)+q48*se - (-(Vy/Izz)*(-tskin*r*sb*sb*sb/(6*l) + astr*strloc[8,1]*sb)+q48*sb)
 #from boom 9 to bottom of spar, so (9*bstring-(pi*r/2)-l)<s<l
-S4 = -(Vy/Izz)*(-tskin*r*s*s*s/(6*l) + astr*strloc[9,1]*s)+q49*s
+sb = (9*bstring-(m.pi*r/2)-l)
+se = l
+S4 = -(Vy/Izz)*(-tskin*r*se*se*se/(6*l) + astr*strloc[9,1]*se)+q49*se - (-(Vy/Izz)*(-tskin*r*sb*sb*sb/(6*l) + astr*strloc[9,1]*sb)+q49*sb)
 #from top of spar to boom 10, so 0<s<((pi*r/2)-bstring)
 #and from boom 10 to end, so ((pi*r/2)-bstring)<s<(pi*r/2)
 #this part is equal the first two, so S610=S1, and S6=S11
 S610 = S1
 S6 = S11
-
 #zsc is the distance from the centre of the spar to the SC, so if the 
 #SC wrt the origin is desired the distance r has to be added to it
 #zsc with q01 and q02
