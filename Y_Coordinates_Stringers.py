d7d3639d366611ba80a19cfe7614c68fb3a74620
MartinvanSchie <61149670+MartinvanSchie@users.noreply.github.com>
2020-02-26 23:15:17 +0100

    Update Y_Coordinates_Stringers.py

--- Y_Coordinates_Stringers.py
+++ Y_Coordinates_Stringers.py
@@ -20,16 +20,22 @@ def y_coordinates_stringers(Ca, h, n_stringer):
     str_on_circ     = int(l_circ/d_stringer)
     str_on_straight = int((n_stringer)/2 - str_on_circ)
     strloc          = np.zeros(str_on_circ+str_on_straight+1)
+    strlocc         = np.zeros(nst)
     strname         = np.empty(str_on_circ+str_on_straight+1,dtype=str)
 
     for i in range(1,str_on_circ+1):
         y = np.sin( (d_stringer*i)/l_circ * np.pi/2 )*R
         strloc[i] = y
+        strlocc[i] = -1*y
         strname[i] = 'c'
 
     for i in range(str_on_straight):
         y = (l_circ + l_straight - ( (str_on_circ+1)*d_stringer + i*d_stringer ))*(R)/l_straight        
         strloc[i+str_on_circ+1] = y
+        strlocc[i+str_on_circ+1] = -1*y
         strname[i+str_on_circ+1] = 's'
+    
+    for i in range(1,(int(nst/2)+1)):
+        strlocc [nst-i] = strloc [i]
 
-    return strloc, strname, l_straight
+    return strloc, strname, l_straight, strlocc
