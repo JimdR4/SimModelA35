8bb9737527a03fda171c616a97dfbcf778a8d320
MartinvanSchie <61149670+MartinvanSchie@users.noreply.github.com>
2020-02-26 23:42:08 +0100

    Update Shear_Flow_S_z.py

--- Shear_Flow_S_z.py
+++ Shear_Flow_S_z.py
@@ -9,8 +9,8 @@ import numpy as np
 from Z_Coordinates_Stringers import z_coordinates_stringers
 from MOI_and_Centroid import MOI_and_centroid
 
-def q_b12(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, omega1):
-    ''' Input:      S_z = shear force in z direction
+def q_b12z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, omega1):
+    ''' Input:      R1z,RIz,R2z,P,R3z,x = shear force in z direction inputs
                     I_yy = moment of inertia around the y axis
                     nst = the number of stringers used
                     Ca = lenth of chord
@@ -37,14 +37,14 @@ def q_b12(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, omega1):
             z = -(1-np.cos(omega))*r - zsc
             z_co, str_loc = z_coordinates_stringers(Ca, ha, nst)[:2]
 
-            val = -S_z/I_yy * (-tsk*r**2*omega + tsk*r**2*np.sin(omega) - tsk*zsc*r*omega + 0.5*B*(z_co[0]-zsc))
+            val = -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy * (-tsk*r**2*omega + tsk*r**2*np.sin(omega) - tsk*zsc*r*omega + 0.5*B*(z_co[0]-zsc))
 
             z_glob_c = z + zsc
 
             for i in range(len(z_co)):
                 if str_loc[i] == 'c':
                     if abs(z_glob_c) >= abs(z_co[i]): 
-                        val = val + -S_z/I_yy* B* (z_co[i]-zsc)
+                        val = val + -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy* B* (z_co[i]-zsc)
             
             values.append(val)
 
@@ -55,19 +55,19 @@ def q_b12(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, omega1):
         r = h/2
         z = -(1-np.cos(omega1))*R - zsc
         z_co, str_loc = z_coordinates_stringers(Ca, ha, nst)[:2]
-        values = -S_z/I_yy * (-tsk*r**2*omega1 + tsk*r**2*np.sin(omega1) - tsk*zsc*r*omega1 + 0.5*B*(z_co[0]-zsc))
+        values = -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy * (-tsk*r**2*omega1 + tsk*r**2*np.sin(omega1) - tsk*zsc*r*omega1 + 0.5*B*(z_co[0]-zsc))
 
         z_glob_co = z + zsc
 
         for i in range(len(z_co)):
             if str_loc[i] == 'c':
                 if abs(z_glob_co) >= abs(z_co[i]): 
-                    values = values + -S_z/I_yy* B* (z_co[i]-zsc)
+                    values = values + -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy* B* (z_co[i]-zsc)
 
     return values 
 
-def q_b42(S_z, I_yy, ha, tsp, zsc, y1):
-    ''' Input:      S_z = shear force in z direction
+def q_b42z(R1z,RIz,R2z,P,R3z,x, I_yy, ha, tsp, zsc, y1):
+    ''' Input:      R1z,RIz,R2z,P,R3z,x = shear force in z direction inputs
                     I_yy = moment of inertia around the y axis
                     ha = height of aileron
                     tsp = spar thickness
@@ -89,7 +89,7 @@ def q_b42(S_z, I_yy, ha, tsp, zsc, y1):
             if y < 0 or y > r:
                 raise ValueError('y should have a vale between 0 and R')
 
-            val = -S_z/I_yy * (-r*tsp*y - zsc*tsp*y)
+            val = -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy * (-r*tsp*y - zsc*tsp*y)
 
             values.append(val)
 
@@ -98,12 +98,12 @@ def q_b42(S_z, I_yy, ha, tsp, zsc, y1):
         if y1 < 0 or y1 > r:
             raise ValueError('y should have a vale between 0 and R')
 
-        values = -S_z/I_yy * (-r*tsp*y1 - zsc*tsp*y1)
+        values = -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy * (-r*tsp*y1 - zsc*tsp*y1)
 
     return values 
 
-def q_b23(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, s1):
-    ''' Input:      S_z = shear force in z direction
+def q_b23z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, s1):
+    ''' Input:      R1z,RIz,R2z,P,R3z,x = shear force in z direction inputs
                     I_yy = moment of inertia around the y axis
                     nst = the number of stringers used
                     Ca = lenth of chord
@@ -132,14 +132,14 @@ def q_b23(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, s1):
                 raise ValueError('s1 should have a value between 0 and length of the straight skin')
 
             z = -r-zsc - (Ca-r)/l_sk * s
-            val = -S_z/I_yy * (-tsk*r*s - tsk*zsc*s - tsk*(Ca-r)/(2*l_sk)*s**2) + q_b12(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, np.pi/2) + q_b42(S_z, I_yy, ha, tsp, zsc, r)
+            val = -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy * (-tsk*r*s - tsk*zsc*s - tsk*(Ca-r)/(2*l_sk)*s**2) + q_b12z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, np.pi/2) + q_b42z(R1z,RIz,R2z,P,R3z,x, I_yy, ha, tsp, zsc, r)
 
             z_glob_c = z+zsc
 
             for i in range(len(z_co)):
                 if str_loc[i] == 's':
                     if abs(z_glob_c) >= abs(z_co[i]): 
-                        val = val + -S_z/I_yy*B*(z_co[i]-zsc)
+                        val = val + -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy*B*(z_co[i]-zsc)
             
             values.append(val)
 
@@ -151,19 +151,19 @@ def q_b23(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, s1):
             raise ValueError('s1 should have a value between 0 and length of the straight skin')
 
         z = -r-zsc - (Ca-r)/l_sk * s1
-        values = -S_z/I_yy * (-tsk*r*s1 - tsk*zsc*s1 - tsk*(Ca-r)/(2*l_sk)*s1**2) + q_b12(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, np.pi/2) + q_b42(S_z, I_yy, ha, tsp, zsc, r)
+        values = -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy * (-tsk*r*s1 - tsk*zsc*s1 - tsk*(Ca-r)/(2*l_sk)*s1**2) + q_b12z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, np.pi/2) + q_b42z(R1z,RIz,R2z,P,R3z,x, I_yy, ha, tsp, zsc, r)
 
         z_glob_co = z+zsc
 
         for i in range(len(z_co)):
             if str_loc[i] == 's':
                 if abs(z_glob_co) >= abs(z_co[i]): 
-                    values = values + -S_z/I_yy*B*(z_co[i]-zsc)
+                    values = values + -Sz_x(R1z,RIz,R2z,P,R3z,x)/I_yy*B*(z_co[i]-zsc)
 
     return values
 
-def q_b35(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, s2):
-    ''' Input:      S_z = shear force in z direction
+def q_b35z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, s2):
+    ''' Input:      R1z,RIz,R2z,P,R3z,x = shear force in z direction inputs
                     I_yy = moment of inertia around the y axis
                     nst = the number of stringers used
                     Ca = lenth of chord
@@ -192,14 +192,14 @@ def q_b35(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, s2):
         if s2 < 0 or s2> l_sk:
             raise ValueError('s1 should have a value between 0 and length of the straight skin') 
     
-    values = q_b23(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, l_sk-s2)
+    values = q_b23z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, l_sk,s2)
     values = np.asarray(values)
     values = values*-1
 
     return values 
 
-def q_b54(S_z, I_yy, ha, tsp, zsc, y2):
-    ''' Input:      S_z = shear force in z direction
+def q_b54z(R1z,RIz,R2z,P,R3z,x, I_yy, ha, tsp, zsc, y2):
+    ''' Input:      R1z,RIz,R2z,P,R3z,x = shear force in z direction inputs
                     I_yy = moment of inertia around the y axis
                     ha = height of aileron
                     tsp = spar thickness
@@ -225,14 +225,14 @@ def q_b54(S_z, I_yy, ha, tsp, zsc, y2):
         if y2 > 0 or y2 < -r:
             raise ValueError('y should have a vale between -R and 0')
 
-    values = q_b42(S_z, I_yy, ha, tsp, zsc, -1*y2)
+    values = q_b42z(R1z,RIz,R2z,P,R3z,x, I_yy, ha, tsp, zsc, -1*y2)
     values = np.asarray(values)
     values = values*-1
 
     return values
 
-def q_b51(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, omega2):
-    ''' Input:      S_z = shear force in z direction
+def q_b51z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, omega2):
+    ''' Input:      R1z,RIz,R2z,P,R3z,x = shear force in z direction inputs
                     I_yy = moment of inertia around the y axis
                     nst = the number of stringers used
                     Ca = lenth of chord
@@ -257,7 +257,7 @@ def q_b51(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, omega2):
         if omega2 > 0 or omega2 < -np.pi/2:
             raise ValueError('Omega1 should have a value between -pi/2 and 0')
 
-    values = q_b12(S_z, I_yy, nst, Ca, ha, tsk, zsc, B, -1*omega2)
+    values = q_b12z(R1z,RIz,R2z,P,R3z,x, I_yy, nst, Ca, ha, tsk, zsc, B, -1*omega2)
     values = np.asarray(values)
     values = values*-1
 
