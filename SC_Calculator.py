79ef7cf06512f333cc25410fb58374c7abad82e5
chumbuks <61149713+chumbuks@users.noreply.github.com>
2020-02-27 12:33:35 +0100

    Updated  SC_Calculator.py
    Increased the stepsize in integration to give more accurate results

--- SC_Calculator.py
+++ SC_Calculator.py
@@ -13,20 +13,20 @@ alpha = np.arcsin(R/l_straight_skin)
 
 
 
-F12=val_simpsons_rule_integration(omega1,1001,shear_flow_1)
-F42=val_simpsons_rule_integration(y_1,1001,shear_flow_1)
-F23=val_simpsons_rule_integration(s1,1001,shear_flow_1)
-F35=val_simpsons_rule_integration(s2,1001,shear_flow_1)
-F54=val_simpsons_rule_integration(y_2,1001,shear_flow_1)
-F61=val_simpsons_rule_integration(omega2,1001,shear_flow_1)
+F12=val_simpsons_rule_integration(omega1,3001,shear_flow_1)
+F42=val_simpsons_rule_integration(y_1,3001,shear_flow_1)
+F23=val_simpsons_rule_integration(s1,3001,shear_flow_1)
+F35=val_simpsons_rule_integration(s2,3001,shear_flow_1)
+F54=val_simpsons_rule_integration(y_2,3001,shear_flow_1)
+F61=val_simpsons_rule_integration(omega2,3001,shear_flow_1)
 
 
 A = np.array([[(np.pi*R/t_sk)+2*R/t_sp,-2*R/t_sp],
              [-2*R/t_sk,l_straight_skin/t_sk+2*R/t_sp]])
 
-B = np.array([[-1*((F12/t_sk)-(F42/t_sp)+(F54/t_sp)+(F61/t_sk))],
-               [-1*((F23/t_sk)+(F35/t_sk)+(F54/t_sp)-(F42/t_sp))]])
-                
+B = np.array([[-1*((F12/t_sk)-(F42/t_sp)-(F54/t_sp)+(F61/t_sk))],
+               [-1*((F23/t_sk)+(F35/t_sk)+(F54/t_sp)+(F42/t_sp))]])
+
                 
 C = np.linalg.solve(A,B)
 
